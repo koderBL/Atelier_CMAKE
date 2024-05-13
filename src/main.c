6deaba434fc6fe3a1fccf3bbe4f565bd69bd3cfe
koderBL <45111974+koderBL@users.noreply.github.com>
2024-05-13 10:11:24 +0000

    test final pour carre

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[]){
 	char* a = argv[2];
 	char* b = argv[3];
 	double r = 0;
+
 	if (strcmp(op, "add") == 0) {
 		r = _add(atof(a), atof(b));
 		printf("%lf",r);
@@ -26,12 +27,21 @@ int main(int argc, char *argv[]){
 		r = _div(atof(a), atof(b));
 		printf("%lf",r);
 	}
-	else if (strcmp(op, "carre") == 0){
+	// else if (strcmp(op, "carre") == 0){
+	// 	r = _carre(atof(a));
+	// 	printf("%lf",r);
+	// }
+	else {printf("Erreur de parametres");}
+	}
+
+	else {
+		char* op = argv[1];
+		char* a = argv[2];
+	    double r = 0;
 		r = _carre(atof(a));
 		printf("%lf",r);
 	}
-	else {printf("Erreur de parametres");}
-	}
+
 	else {printf("Erreur de parametres");}
 	return 0;
 }
